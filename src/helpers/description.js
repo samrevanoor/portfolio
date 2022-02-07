0cfb6ce4c78a08d1a28ac092108c3b5a8205c67a
Sam Revanoor <sam@trint.com>
2022-02-07 20:35:04 +0000

    sets up homepage for portfolio website

---
+++ src/helpers/description.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+
+export const description = {
+  name: "Sam Revanoor",
+  job: "Associate Software Engineer at Trint",
+  tech_stack: [
+    "javascript",
+    "react",
+    "node",
+    "express",
+    "mongodb",
+    "python",
+    "django",
+  ],
+  github: "/samrevanoor",
+};
---
+++ src/layouts/Main.jsx
@@ -0,0 +1,65 @@
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="content">
+          <span className="title">description</span> ={" "}
+          <span className="brace">{"{"}</span> <br />
+          &nbsp; <span className="title">name:</span>{" "}
+          <span className="value">"sam revanoor"</span>, <br />
+          &nbsp; <span className="title">job:</span>{" "}
+          <span className="value">"associate software engineer at trint"</span>
+          , <br />
+          &nbsp; <span className="title">tech_stack:</span>{" "}
+          <span className="brace">{"["}</span>
+          <span className="value">"javascript"</span>,
+          <span className="value">"react"</span>,{" "}
+          <span className="value">"node"</span>,{" "}
+          <span className="value">"express"</span>,{" "}
+          <span className="value">"mongodb"</span>,{" "}
+          <span className="value">"python"</span>,{" "}
+          <span className="value">"django"</span>
+          <span className="brace">{"]"}</span>, <br />
+          &nbsp; <span className="title">github:</span>{" "}
+          <span className="value">
+            <a
+              href="https://github.com/samrevanoor/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              "/samrevanoor"
+            </a>
+          </span>
+          , <br />
+          &nbsp; <span className="title">projects:</span>{" "}
+          <span className="brace">{"["}</span>
+          <span className="value">
+            <a
+              href="https://wordlez.samrevanoor.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              "wordlez"
+            </a>
+          </span>
+          ,
+          <span className="value">
+            <a
+              href="https://jeopardy.samrevanoor.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              "game of thrones jeopardy"
+            </a>
+          </span>
+          <span className="brace">{"]"}</span>, <br />
+          <span className="brace">{"}"}</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
